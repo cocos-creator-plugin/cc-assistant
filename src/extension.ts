a686de2bec2a5bfae33902f493972dfe16f34930
xyf-mac <xu_yanfeng@126.com>
2020-10-08 19:08:51 +0800

    init

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,58 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+const packageName = "assistant";
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('插件激活了');
-	let disposable = vscode.commands.registerCommand('kitty.helloWorld', () => {
-		console.log('3333333333333333300033');
-		vscode.window.showInformationMessage('Hello World from kitty! 66666');
-	});
-	context.subscriptions.push(disposable);
+	vscode.window.showInformationMessage("欢迎使用，有问题随时联系小王子[QQ 774177933]");
 
-	let dd = vscode.commands.registerCommand('kitty.go', () => {
-		console.log("kitty go"); 
-	})
-	context.subscriptions.push(dd)
+	let cmdConfig = [
+		{
+			cmd: `${packageName}.openSell`,
+			cb: () => {
+				let site = 'https://store-my.cocos.com/#/seller/history';
+				vscode.env.openExternal(vscode.Uri.parse(site));
+			}
+		},
+		{
+			cmd: `${packageName}.openForum`,
+			cb: () => {
+				vscode.window.showInputBox({
+					placeHolder: '请输入论坛搜索内容',
+					prompt: '无搜索内容可以忽略',
+				}).then((msg) => {
+					if (msg !== undefined) {
+						let site = "https://forum.cocos.org";
+						if (msg.length === 0) {
+							vscode.env.openExternal(vscode.Uri.parse(site));
+						} else {
+							vscode.env.openExternal(vscode.Uri.parse(`${site}/search?q=${msg}`));
+						}
+					}
+				});
+			},
+		},
+		{
+			cmd: `${packageName}.openDocs`,
+			cb: () => {
+				vscode.window.showInputBox({
+					placeHolder: '请输入文档搜索内容',
+					prompt: '无搜索内容可以忽略'
+				}).then((msg) => {
+					let site = "https://docs.cocos.com/creator/manual/zh";
+					if (msg !== undefined) {
+						if (msg.length === 0) {
+							vscode.env.openExternal(vscode.Uri.parse(site));
+						} else {
+							vscode.env.openExternal(vscode.Uri.parse(`${site}/?q=${msg}`));
+						}
+					}
+				});
+			},
+		},
+	];
+	cmdConfig.forEach(({ cmd, cb }) => {
+		let disposable = vscode.commands.registerCommand(cmd, cb);
+		context.subscriptions.push(disposable);
+	});
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() { }
